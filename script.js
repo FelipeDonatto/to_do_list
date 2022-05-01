a431f961cac8e36b62be755108132d66b26018c3
Felipe <fe.donatto@outlook.com>
2022-05-01 17:23:43 -0300

    8 feito

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ button.addEventListener('click', createTask)
 
 function grayBg(event){
     if(event.target.id != 'lista-tarefas'){
+    const allLi = document.querySelectorAll('li').length
+    const whitebg = document.querySelectorAll('li')
+    if(allLi > 0){
+        for(let i = 0; i<allLi; i+= 1){
+            whitebg[i].style.backgroundColor = 'white'
+        }
+    }
     const target = event.target
     target.style.backgroundColor = 'grey'
 }
